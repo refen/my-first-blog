7fa75ab6ad507aba4402c034240796773a13e779
Your Name <517211901@qq.com>
2015-09-20 10:15:44 +0800

    Add view and template for detailed blog post as well as Css for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
@@ -8,3 +8,8 @@ def post_list(request):
     # posts = Post.objects.filter(title__contains='title')
     posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    posts = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': posts})
