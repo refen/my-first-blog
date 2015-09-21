cdff73961a955bd24957c91f4c30425f5240461f
Your Name <517211901@qq.com>
2015-09-21 21:58:28 +0800

    Implementing the function of comment

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,12 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+admin.autodiscover()
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('blog.urls')),
-]
+urlpatterns = patterns('',
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+                       url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+                       url(r'', include('blog.urls')),
+                       )
+
